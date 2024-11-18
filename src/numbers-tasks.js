75a307d9b1bf01a56a82dba70d413cec15b0135d
chorny-znich <vargar.dt@gmail.com>
2024-11-18 09:29:50 +0300

    feat: 15 tasks done

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -225,8 +225,12 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const num = Number(value);
+  if (Number.isNaN(num)) {
+    return def;
+  }
+  return num;
 }
 
 /**
@@ -240,8 +244,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
